b6fac913e30cbe213ea9ee61996f70cc0f29d89c
eershalaim <eershalaim@gmail.com>
2022-04-11 22:58:33 +0300

    fix checkout controller - mapping found items

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -7,9 +7,11 @@ class CheckoutController < ApplicationController
   end
 
   def summary
-    @items = Item.find(params[:items])
+    #TODO validate param
+    item_entities = Item.where(code: params[:items])
+    items = params[:items].map{ |p| item_entities.find {|e| e.code == p}}
     basket_campaigns = PromoCampaign.find_by_campaign_type(CheckoutHelper::CAMPAIGN_TYPE[:good_discount])
-    summary = BasketService.calc_basket_summary(@items, basket_campaigns)
+    summary = BasketService.calc_basket_summary(items, basket_campaigns)
     return render json: summary.nil? ? { error: 'Basket summarising failed' } : summary
   end
 end
