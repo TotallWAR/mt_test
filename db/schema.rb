9828aeaffd007eb4b9dd807ae5a72c8f8f006d1a
eershalaim <eershalaim@gmail.com>
2022-04-11 21:39:23 +0300

    update db scheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_154453) do
+ActiveRecord::Schema.define(version: 2022_04_11_181025) do
 
   create_table "items", force: :cascade do |t|
     t.string "code"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_04_11_154453) do
     t.datetime "date_to"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "campaign_type"
   end
 
 end
